2ecaa6a22eebfe2764fafb5fda9e27dd49383bf3
unknown <ganeshyadavstudies@gmail.com>
2023-01-18 19:54:52 +0530

    added z- index

--- navbar.js
+++ navbar.js
@@ -1,21 +1,23 @@
-// const swiper = new Swiper('.swiper', {
-//     autoplay: {
-//         delay: 2000,
-//         disableOnIntraction: false,
-//     },
-//     loop: true,
 
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
 
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 2000,
+         disableOnIntraction: false,
+    },
+    loop: true,
 
-// });
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+});
 
 // let login = document.getElementById("login")
 // login.addEventListener("click",()=>{
